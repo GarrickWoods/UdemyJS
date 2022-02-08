fa3194096a430bd62936bffe316bc8112c601669
Garrick Woods <gwoods@thehouseofdesign.com>
2022-02-08 15:46:40 -0700

    wow

---
+++ 01-Fundamentals-Part-1/starter/Script copy.js
@@ -0,0 +1,35 @@
+ /*
+ let JS = 'amazing';
+ if (JS === 'amazing') alert('Stuff it');
+
+ 40 + 8 + 23 - 10;
+ console.log(40 + 8 + 23 - 10);
+
+let n = '1' + 1;
+n = n-1;
+console.log(n);
+
+*/
+
+// 5 falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 100;
+if(money){
+    console.log("Don't spend any money");
+}else{
+    console.log("You should get a job!");
+}
+
+let height = 1;
+if(height){
+    console.log(`yay! Height is defined as ${height} Meters`)
+}else{
+    console.log('Oh no! Height is undefined!')
+}
+
+
--- 01-Fundamentals-Part-1/starter/Script.js
+++ 01-Fundamentals-Part-1/starter/Script.js
@@ -1,5 +1,5 @@
 let JS = 'amazing';
-if (JS === 'amazing') alert('Suck a Dick');
+if (JS === 'amazing') alert('Suck it');
 
 40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
