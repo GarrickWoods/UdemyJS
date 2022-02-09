85376e6a3567e176882345acd9c93e3c9eaa1a4a
GarrickWoods <99113027+GarrickWoods@users.noreply.github.com>
2022-02-08 22:12:28 -0700

    changes

--- 01-Fundamentals-Part-1/starter/Script.js
+++ 01-Fundamentals-Part-1/starter/Script.js
@@ -2,12 +2,9 @@
 /*
  let JS = 'amazing';
 if (JS === 'amazing') alert('Suck It');
-
 40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
-*/
 
-/*
 const birthyear = 1995;
 let century;
 
@@ -18,9 +15,7 @@ if (birthyear <= 2000) {
 }
 console.log(birthyear);
 console.log(century);
-*/
 
-/*
 const massMark = 78;
 const heightMark = 1.69;
 const massJohn = 95;
@@ -41,7 +36,6 @@ if (BMIMark > BMIJohn) {
     console.log(`It is ${markHigherBMI} that Mark is a Fatass`);
     console.log(`Mark's BMI is ${BMIMark.toFixed(2)}. John's is ${BMIJohn.toFixed(2)}.`);
 }
-*/
 
 //type conversion
 const inputYear = '1991';
@@ -56,3 +50,36 @@ console.log('I am ' + 23 + ' years old');
 let n = '1' + 1;
 n = n - 1;
 console.log(n);
+
+// 5 falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 100;
+if (money) {
+    console.log("Don't spend any money");
+} else {
+    console.log("You should get a job!");
+}
+
+let height = 1;
+if (height) {
+    console.log(`yay! Height is defined as ${height} Meters`)
+} else {
+    console.log('Oh no! Height is undefined!')
+}
+
+*/
+
+
+let howManyHoursTotal = (5 + 4 + 2 + 1 + 4 + 3 + 5.5 + 2.5 + 6 + 3 + 5 + 4 + 4 + 5 + 4 + 8 + 1);
+console.log(`It will take me at least ${howManyHoursTotal.toFixed(1)} Hours to complete this course.`);
+
+let videoWatchSpeed = 1.5;
+let howManyHoursPerDay = 3;
+let howManyDays = (howManyHoursTotal / howManyHoursPerDay) / videoWatchSpeed;
+let howManyWeeks = howManyDays / 7;
+console.log(`Which is ${howManyDays.toFixed(1)} days, or ${howManyWeeks.toFixed(1)} weeks at ${howManyHoursPerDay.toFixed(1)} hours per day, at video watch speed of ${videoWatchSpeed.toFixed(2)}.`);
