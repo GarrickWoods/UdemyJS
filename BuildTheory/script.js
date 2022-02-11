a8b206768472a573ecce49dc9c76931ce8400603
Garrick Woods <gwoods@thehouseofdesign.com>
2022-02-11 11:00:56 -0700

    day 5 halfway

---
+++ BuildTheory/script.js
@@ -0,0 +1,26 @@
+/* The start of my endeavor to make the website "build-theory.com"
+
+This will be an interactive website, free to users where they can build and customize 3D models of
+their vehicles, making them exactly the way they've always dreamed.
+
+I intend to start out in the off-road niche with vehicles and parts. The 3 starting models will be 
+a 3rd gen Tacoma, a 5th gen 4Runner, and a 4 door Jeep Wrangler. 
+
+Companies will eventually pay and provide 3d models to list their parts on our website. 
+They will also be able to pay extra to sponsor their products on the site, or list them first among parts
+that the users see in various part categories. 
+
+There will be a basic front page, showing what the site does and maybe highlighting new additions to the site, 
+with a link to the "build Theory visualizer" where they can build their own vehicle(s). 
+
+Eventually this site would serve as a resource for vehicle enthusiasts (mostly in the off-road community initially) 
+in the form of video tests and reviews of specific parts, written articles, and forums for users to answer others' questions. 
+
+I would hope to eventually expand the site to include many kinds of cars, but that expansion will be based heavily 
+demand within the user community. 
+
+*/
+
+'use strict';
+
+
