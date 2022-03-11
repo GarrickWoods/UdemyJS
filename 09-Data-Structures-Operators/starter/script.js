37bad9d67e4a6705ab569969339db6db24e1522c
Garrick Woods <gwoods@thehouseofdesign.com>
2022-03-10 20:41:06 -0700

    JROPKFE

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function(starterIndex, mainIndex) {
+    return[this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,77 @@ const restaurant = {
     },
   },
 };
+
+// destructuring the object
+const {name, openingHours, categories} = restaurant;
+console.log(name, openingHours, categories);
+
+const{
+  name: restaurantName, 
+  openingHours: hours, 
+  categories: tags
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// setting default values 
+const {menu = [], starterMenu: starters = []} = restaurant;
+
+// mutating variables
+let aa = 111;
+let bb = 999;
+const obj = {aa: 23, bb: 7, cc: 14};
+
+({aa, bb} = obj);
+console.log(aa, bb);
+
+//nested objects 
+const{ 
+  fri: {open: op, close: cl},
+} = openingHours;
+
+console.log(op, cl);
+
+
+
+
+
+
+
+// declare variables separately with an array
+const arr = [2,3,4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+// same as above but all at once
+const [x,y,z] = arr;
+console.log(x,y,z);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+
+// switch main adn secondary without restructuring
+const temp = main;
+main = secondary;
+secondary = temp;
+console.log(main, secondary);
+
+// same as above with destructuring
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+
+//receive 2 return values from a function
+ const [starter, mainCourse] = restaurant.order(2, 0);
+
+// nested arrays
+ const nested = [2, 4, [5, 6]];
+
+const [i,, [j, k]] = nested;
+console.log(i, j, k);
+
+// default values 
+const [p=1, q=1, r=1] = [8,9];
+console.log(p, q, r);
+
